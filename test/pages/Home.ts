587872e33861b4f5b6799ce302a040a2dabe2b19
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-06-12 13:29:10 -0700

    Added isDisplayed to the Base Page

--- test/pages/Home.ts
+++ test/pages/Home.ts
@@ -2,6 +2,14 @@ import { Page } from "./Page.ts";
 
 class Home extends Page {
   url = "/";
+
+  get mainHeader() {
+    return $("h1.heading");
+  }
+
+  async isDisplayed() {
+    return (await this.mainHeader.getText()) === "Welcome to the-internet";
+  }
 }
 
 export const home = new Home();
--- test/pages/Page.ts
+++ test/pages/Page.ts
@@ -1,6 +1,8 @@
 export abstract class Page {
   protected abstract url: string;
 
+  abstract isDisplayed(): Promise<boolean>;
+
   async open() {
     await browser.url(this.url);
   }
